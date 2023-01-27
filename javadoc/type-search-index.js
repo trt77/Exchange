888fb3e9ec42f2cefc372f2f04a4a334b6c1a584
trt77 <hubertsoroczynski@gmail.com>
2023-01-27 21:21:02 +0100

    add javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ConnectionDetails"},{"p":"<Unnamed>","l":"CurrencyPair"},{"p":"<Unnamed>","l":"CurrencyPairWindow"},{"p":"<Unnamed>","l":"Exchange"}];updateSearchResults();
